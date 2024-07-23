3d5c3ba9b0aebe1977dd6af3672c724983ecae26
Muratcan Aşgün <masgun13@ku.edu.tr>
2024-07-23 22:36:40 +0300

    dummy relation line
    a dummy, thick and transparent line is added for every relation line to capture onHover easier

--- functions/functions.js
+++ functions/functions.js
@@ -7,38 +7,48 @@ import { useState } from "react"
 const langColors = {
     "AR": ["bg-lime-500", "Arabic"],
     "TR": ["bg-red-300", "Turkish"],
-    "EN": ["bg-sky-300","English"],
-    "FR": ["bg-indigo-300","French"],
-    "LA": ["bg-red-600","Latin"],
-    "IT": ["bg-green-400","Italian"],
-    "GR": ["bg-sky-500","Greek"]
+    "EN": ["bg-sky-300", "English"],
+    "FR": ["bg-indigo-300", "French"],
+    "LA": ["bg-red-600", "Latin"],
+    "IT": ["bg-green-400", "Italian"],
+    "GR": ["bg-sky-500", "Greek"]
 
 }
 
 
-const DrawRelation = ({x1, x2, heightOffset, y, depthDiff, pair, setHoveredPair}) => {
+const DrawRelation = ({ x1, x2, heightOffset, y, depthDiff, pair, setHoveredPair }) => {
 
     const [lineColor, setLineColor] = useState("#111")
 
-    const setHoverColor = () =>{
+    const setHoverColor = () => {
         setLineColor("#f77")
         setHoveredPair(pair)
         console.log(pair);
-    } 
-    const revertHoverColor = () =>{
+    }
+    const revertHoverColor = () => {
         setLineColor("#111")
-        setHoveredPair([-1,-1])
+        setHoveredPair([-1, -1])
     }
 
-    return <svg className="absolute overflow-visible z-0"><line  
-        onMouseEnter={()=> setHoverColor()} 
-        onMouseLeave={() => revertHoverColor()}
-        x1={x1}
-        y1={heightOffset}
-        x2={x2}
-        y2={heightOffset + y *2 }
-        stroke={lineColor}
-        strokeWidth={5}></line></svg>
+    return <svg className="absolute overflow-visible z-0">
+        <line
+            onMouseEnter={() => setHoverColor()}
+            onMouseLeave={() => revertHoverColor()}
+            x1={x1}
+            y1={heightOffset}
+            x2={x2}
+            y2={heightOffset + y * 2}
+            stroke={"transparent"}
+            strokeWidth={20}></line>
+        <line
+ 
+            x1={x1}
+            y1={heightOffset}
+            x2={x2}
+            y2={heightOffset + y * 2}
+            stroke={lineColor}
+            strokeWidth={1}></line>
+        </svg>
 
 }
 
@@ -46,4 +56,4 @@ const DrawRelation = ({x1, x2, heightOffset, y, depthDiff, pair, setHoveredPair}
 
 
 
-export { DrawRelation , langColors}
+export { DrawRelation, langColors }
