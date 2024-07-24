d6fa0ac40a5685ac30bf8864c06f8468293fd0e2
Muratcan Aşgün <masgun13@ku.edu.tr>
2024-07-25 00:18:35 +0300

    implements insertion of a word in between
    and implements a depth recalculation algorithm

--- components/createWordDiv.js
+++ components/createWordDiv.js
@@ -2,9 +2,9 @@
 import { langColors } from "@/functions/functions";
 import { useEffect } from "react";
 
-const reqFields = [["key", ""], ["lang", ""],["original",""]]
+const reqFields = [["key", ""], ["lang", ""], ["original", ""]]
 const auxiliaryField = [["desc", ""], ["type", ""]]
-const autoReqFields = [["id", 0], ["depth", 0] ]
+const autoReqFields = [["id", 0], ["depth", 0]]
 /*, ["rel", {
     "derives": {},
     "loans": {},
@@ -13,91 +13,148 @@ const autoReqFields = [["id", 0], ["depth", 0] ]
 
 let fields = reqFields.concat(auxiliaryField, autoReqFields)
 
-export default function CreateWordDiv({ newWordData, setNewWordData, relation, wordPrev, newId, allWords, setAddingData, setFilteredData,unsavedWordCount,setUnsavedWordCount}) {  // Manual inversion of FROM and TO. Careful!
+export default function CreateWordDiv({ newWordData, setNewWordData, relation, wordPrev, newId, allWords, setAddingData, isInsertMode,
+    insertionRelations, setMustDepthRecalculate, hoveredPair, setIsInsertMode, setFilteredData, unsavedWordCount, setUnsavedWordCount }) {  // Manual inversion of FROM and TO. Careful!
 
-    
-    useEffect( () => {
+
+    useEffect(() => {
         let WordDefault = {}
         for (const field of fields) {
             WordDefault[field[0]] = field[1]
         }
-        WordDefault["rel"] = {"derives": {},"loans": {},"homonym": {}}
+        WordDefault["rel"] = { "derives": {}, "loans": {}, "homonym": {} }
         // FIND ID
         WordDefault["id"] = newId
 
-        // FIND DEPTH
-        const relationTypeUpDown =  relation[1]
-        const depthIncrement = relationTypeUpDown==="to" ? 1 : -1
-        const parentWordDepth = wordPrev["depth"]
-        WordDefault["depth"] = parentWordDepth + depthIncrement
 
-        console.log(WordDefault);
-        // FIND RELATION
-        const oppositeRelation = relationTypeUpDown === "to" ? "from" : "to"
-        const relName = relation[0]
-        WordDefault["rel"][relName][oppositeRelation] = [wordPrev["id"]]
 
         console.log(WordDefault);
         //console.log(relation)
         //console.log(wordPrev);
         setNewWordData(WordDefault)
-    },[newId])
+    }, [newId])
 
 
     const handleInputChange = (key, e) => {
-        
+
         console.log("pressed");
         console.log([key, e.target.value]);
 
-        const newDict = { ...newWordData}
-        newDict[key]  = e.target.value
+        const newDict = { ...newWordData }
+        newDict[key] = e.target.value
         setNewWordData(newDict)
-        
+
     }
 
     const saveWordData = () => {
+        // Part 1 modify the new word
+        // FIND DEPTH
+        const relationTypeUpDown = relation[1]
+        const depthIncrement = relationTypeUpDown === "to" ? 1 : -1
+        const parentWordDepth = wordPrev["depth"]
+        newWordData["depth"] = parentWordDepth + depthIncrement
+        if(parentWordDepth+depthIncrement < 0) { 
+            console.log(["RECALCULATE FOR " , newId]);
+            setMustDepthRecalculate(newId)}
 
+        console.log(newWordData);
+        // FIND RELATION
+        const oppositeRelation = relationTypeUpDown === "to" ? "from" : "to"
+        const relName = relation[0]
+        newWordData["rel"][relName][oppositeRelation] = [wordPrev["id"]]
+
+        // Part 2 concat new word to the all word list
         console.log(newWordData);
         const newAllWords = [...allWords]
         newAllWords.push(newWordData)
 
+
+        // Part 3 modify the existing word
         const existingRelation = newAllWords[wordPrev["id"]]["rel"][relation[0]][relation[1]]  //for the existing word, get if the relation is already defined. We will either initialize or extend the list
-        const isRelationDefinedBefore = (existingRelation || []).length > 0 
-        console.log(   [existingRelation,  isRelationDefinedBefore])
-        if(isRelationDefinedBefore){
+        const isRelationDefinedBefore = (existingRelation || []).length > 0
+        console.log([existingRelation, isRelationDefinedBefore])
+        if (isRelationDefinedBefore) {
             newAllWords[wordPrev["id"]]["rel"][relation[0]][relation[1]].push(newId)
-        }else{
+        } else {
             newAllWords[wordPrev["id"]]["rel"][relation[0]][relation[1]] = [newId]
         }
         console.log(newAllWords);
 
         setAddingData(false)
         setFilteredData([newAllWords])
-        setUnsavedWordCount(unsavedWordCount + 1 )
+        setUnsavedWordCount(unsavedWordCount + 1)
         setNewWordData(null)
     }
 
-
-    return <div className="flex flex-col m-4">
-        {reqFields.concat(auxiliaryField).map((x,key_ind) =>
-            <div key={key_ind}> {x[0]} <input onChange={(e) => handleInputChange(x[0],e)}></input> </div>)
+    const insertWordData = () => {
+        const firstRelation = insertionRelations[2][insertionRelations[0]]
+        const secondRelation = insertionRelations[2][insertionRelations[1]]
+        const wordIdUp = hoveredPair[0]
+        const wordIdDown = hoveredPair[1]
+        const depthUp = allWords[wordIdUp]["depth"]
+        
+        // Part 1 prepare the new word
+        newWordData["depth"] = depthUp + 1
+        newWordData["rel"][firstRelation]["from"] = [wordIdUp]
+        newWordData["rel"][secondRelation]["to"] = [wordIdDown]
+        // Part 2 concat new word to the all word list
+        console.log(newWordData);
+        const newAllWords = [...allWords]
+        newAllWords.push(newWordData)
+        // Part 3 delete the previous relation
+        const relationsAll = ["derives","loans","homonym"]
+        for(const rel of relationsAll){ // search for all relations to locate the relation
+            console.log(newAllWords[wordIdUp]["rel"]);
+            console.log(rel);
+            let indToRemove = newAllWords[wordIdUp]["rel"][rel]["to"]?.indexOf(wordIdDown)
+            if(indToRemove > -1){
+                newAllWords[wordIdUp]["rel"][rel]["to"].splice(indToRemove,1)
+            }
+            console.log(newAllWords);
+            console.log([wordIdDown, wordIdUp, newId]);
+            indToRemove = newAllWords[wordIdDown]["rel"][rel]["from"]?.indexOf(wordIdUp)
+            if(indToRemove > -1){
+                newAllWords[wordIdDown]["rel"][rel]["from"].splice(indToRemove,1)
+            }
         }
-        <div className="m-8 border bg-lime-500" onClick={() => saveWordData()}>SAVE WORD</div>
+        // Part 4 modify TO of UP word  
+        //newAllWords[wordIdUp]["rel"][firstRelation]["to"] 
+        const existingRelation = newAllWords[wordIdUp]["rel"][firstRelation]["to"]   //for the existing word, get if the relation is already defined. We will either initialize or extend the list
+        const isRelationDefinedBefore = (existingRelation || []).length > 0
+
+        if (isRelationDefinedBefore) {
+            newAllWords[wordIdUp]["rel"][firstRelation]["to"].push(newId)
+        } else {
+            newAllWords[wordIdUp]["rel"][firstRelation]["to"]  = [newId]
+        }
+        // Part 5 modify FROM of DOWN word 
+        const existingRelation2 = newAllWords[wordIdDown]["rel"][secondRelation]["from"]   //for the existing word, get if the relation is already defined. We will either initialize or extend the list
+        const isRelationDefinedBefore2 = (existingRelation2 || []).length > 0
+
+        if (isRelationDefinedBefore2) {
+            newAllWords[wordIdDown]["rel"][secondRelation]["from"].push(newId)
+        } else {
+            newAllWords[wordIdDown]["rel"][secondRelation]["from"] = [newId]
+        }
+        
+        setAddingData(false)
+        setIsInsertMode(false)
+        setFilteredData([newAllWords])
+        setUnsavedWordCount(unsavedWordCount + 1)
+        setNewWordData(null)
+        setMustDepthRecalculate(newId)
+        console.log(newAllWords);
+    }
+
+    return <div className="flex flex-col m-4 p-2 border border-slate-300 shadow-md">
+        {reqFields.concat(auxiliaryField).map((x, key_ind) =>
+            <div className="flex m-1" key={key_ind}>
+                <span className="flex-1 m-1">{x[0]} </span>
+                <input className="flex-1 rounded" onChange={(e) => handleInputChange(x[0], e)}></input>
+            </div>)
+        }
+        {isInsertMode ? <div className="m-4 p-2 border bg-lime-500 text-center rounded cursor-pointer " onClick={() => insertWordData()}>INSERT WORD</div> :
+            <div className="m-4 p-2 border bg-lime-500 text-center rounded cursor-pointer " onClick={() => saveWordData()}>SAVE WORD</div>}
     </div>
 
-
-
-
-
-
-    //console.log(pos);
-    return <div key={x.id} style={{ left: `${pos[x.id] || 0}px` }}
-        onClick={() => { setSelectedWord(x); setPopupOpen(true) }}
-        className={`word-card-individual  absolute border-2 min-w-32 min-h-24 z-10 text-center  justify-center rounded-lg flex flex-col ${langColors[x.lang][0]} hover:shadow-lg`} >
-        { // absolute
-        }
-        <span>{x.key}</span>
-        <span className="text-2xl"> {x.original}</span>
-        <span className="text-xs"> {x.id}</span>
-    </div>
 }
--- functions/functions.js
+++ functions/functions.js
@@ -5,13 +5,19 @@ import { useState } from "react"
 
 
 const langColors = {
+    "PIE" : ["bg-gradient-to-bl from-indigo-300", "Proto-Indo-European"], 
+    "PGER": ["bg-gradient-to-bl from-orange-300", "Proto-Germanic"],
+
+
     "AR": ["bg-lime-500", "Arabic"],
     "TR": ["bg-red-300", "Turkish"],
     "EN": ["bg-sky-300", "English"],
     "FR": ["bg-indigo-300", "French"],
     "LA": ["bg-red-600", "Latin"],
     "IT": ["bg-green-400", "Italian"],
-    "GR": ["bg-sky-500", "Greek"]
+    "GR": ["bg-sky-500", "Greek"],
+    "DE": ["bg-orange-500", "German"],
+    "FA": ["bg-yellow-500", "Persian"]
 
 }
 
@@ -67,7 +73,70 @@ const DrawRelation = ({ x1, x2, heightOffset, y, depthDiff, pair, setHoveredPair
 }
 
 
+function RecalculateDepthAfter( allWords, nodeId, setFilteredData) {
+
+    // fix the depth for negative 
+    if(allWords[nodeId]["depth"] < 0) {allWords[nodeId]["depth"] = 0}
+
+    let depthDict = {}
+    depthDict[nodeId] = allWords[nodeId]["depth"]
+    let parentDict = {}
+
+    let derivesTo = allWords[nodeId]["rel"]["derives"]["to"] || []
+    let loansTo = allWords[nodeId]["rel"]["loans"]["to"] || []
+    let homonymTo = allWords[nodeId]["rel"]["homonym"]["to"] || []
+    
+    let goToItems = derivesTo.concat(loansTo, homonymTo)
+    // initialize child-parent mapping
+    for(const child of goToItems){
+        parentDict[child] = nodeId
+    }
+    let safety = 0
+    console.log(goToItems);
+    console.log(parentDict);
+    console.log(depthDict);
+    while (goToItems.length > 0) {
+        safety = safety + 1
+        if (safety > 300) { break }
+    
+        console.log(goToItems);
+        //console.log(data);
+        let idToProcess = goToItems[0]
+        let parentDepth =  depthDict[parentDict[idToProcess]]
+        console.log(parentDict);
+        console.log(depthDict);
+        //for (var idNow = 0; idNow < itemTogetherCount; idNow++) {  // Treat the similar siblings together (e.g derived from same root)
+        allWords[idToProcess]["depth"] = parentDepth + 1 
+        depthDict[idToProcess] = parentDepth + 1 
+        
+          //console.log([idsToProcess[idNow], nodeData]);
+          let derivesTo = allWords[idToProcess]["rel"]["derives"]["to"] || []
+          let loansTo = allWords[idToProcess]["rel"]["loans"]["to"] || []
+          let homonymTo = allWords[idToProcess]["rel"]["homonym"]["to"] || []
+    
+          let newGoToItems = derivesTo.concat(loansTo, homonymTo)
+          //console.log([idsToProcess[idNow], newGoToItems]);
+          if (newGoToItems.length > 0) {
+    
+            goToItems.push(...newGoToItems)
+            for(const child of newGoToItems){
+                parentDict[child] = idToProcess
+            }
+        
+           }
+    
+        //}
+        goToItems.shift()           // remove the first element
+        
+        //console.log(goToItems);
+        //console.log(parentPositions);
+    
+      }
+      const newAllWords = [...allWords]
+      console.log(newAllWords);
+      setFilteredData([newAllWords])
+}
 
 
 
-export { DrawRelation, langColors }
+export { DrawRelation, langColors, RecalculateDepthAfter }
