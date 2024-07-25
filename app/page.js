30926a504dc1127f99e6a51bcc49f550e8947cd0
Muratcan Aşgün <masgun13@ku.edu.tr>
2024-07-25 21:44:09 +0300

    etymoball v0
    for the main page, a static, rolling ball of words

--- app/page.js
+++ app/page.js
@@ -1,34 +1,52 @@
 "use client"
-import Image from "next/image";
-import { useEffect, useState, useRef } from "react";
-import WordCard from "@/components/wordCard";
-import SaveToServerButton from "@/components/saveToServerButton";
-import { DrawRelation, langColors, RecalculateDepthAfter } from "@/functions/functions";
-import Legend from "@/components/legend";
-import Popup from "@/components/popup";
-import { useRouter } from 'next/navigation';
-//console.log(data[1]);
 
- 
 
+import { Cairo } from "next/font/google";
+import { useState } from "react";
+export const cairo = Cairo({
+  variable: '--font-cairo',
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "900"]
+})
+
+import Etymoball from "@/components/etymoball";
 
 export default function Home() {
 
  
   return (
-    <main className={`flex min-h-screen flex-col items-center place-content-start p-24 `}>
-    
-      <div className="z-10 mb-12 max-w-5xl w-full items-center justify-center   font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <main className={`flex min-h-screen flex-col items-center place-content-start p-0 ${cairo.className}`}>
 
-          <code className="font-mono font-bold text-4xl">Etymallogy</code>
+      <div className="bg-gray-200 z-10 mb-12 w-full  items-center justify-center    text-sm lg:flex flex-col">   {/* max-w-5xl*/}
+        <p className="  left-0 top-0 flex w-full justify-center border-b
+         pb-6 pt-8 mt-64 mb-64 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  
+        lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"> {/* fixed bg-gradient-to-b   border-gray-300 
+          from-zinc-200 */}
+
+          <span className="  font-bold text-4xl">Etymallogy</span>
         </p>
 
-    
+        <div className="bg-slate-300 left-0 right-0 w-full flex flex-col h-[150svh]">
+          <div className="bg-gradient-to-b from-gray-200 h-1/3 flex flex-col justify-center">
+            <p className=" text-xl self-center m-24 ">An online collection of etymological knowledge</p>
+
+
+            <div className=" text-xl self-center m-4 ">1000+ Turkish words</div>
+            <div className=" text-xl self-center m-4 ">1000+ English words</div>
+            <div className=" text-xl self-center m-4 ">1000+ Arabic words</div>
+            
+          </div>
+          <Etymoball words={["şerbet", "şarap", "meclis", "wine", "şurup", "語", "kitap", "lycée", "bilim", "science",
+            "لغة", "vin", "sorbetto", "λόγος", "lisan", "ستاره", "stella", "ἀστήρ"]}></Etymoball>
+        </div>
+
+
+
+
       </div>
 
 
-   
+
     </main>
   );
 }
---
+++ components/etymoball.js
@@ -0,0 +1,111 @@
+
+import { langColors } from "@/functions/functions";
+import { useEffect, useState } from "react";
+
+
+
+export default function Etymoball({ words }) {
+
+    const [wordsList, setWordsList] = useState(words)
+    const [coordinates, setCoordinates] = useState([])
+    const wordCount = words.length
+    const radius = 200
+    const sy = Math.sin(0.5 * Math.PI / 180); //fy
+    const cy = Math.cos(0.5 * Math.PI / 180); //fy
+    const sx = Math.sin(0.4 * Math.PI / 180); //fx
+    const cx = Math.cos(0.4 * Math.PI / 180); //fx
+    useState(() => {
+
+
+        let phi = 0;
+        let theta = 0;
+        let max = words.length;
+        let i = 0;
+        let newCoordinates = []
+
+        for (const i in words) {
+            const newWordDict = { cx: 0, cy: 1, cz: 1, h: 20, w: 20, per: 1 }
+            phi = Math.acos(-1 + (2 * parseInt(i)) / max);
+            theta = Math.sqrt(max * Math.PI) * phi;
+
+            console.log([i, -1 + (2 * parseInt(i)) / max, max, phi, theta, words]);
+            newWordDict.cx = radius * Math.cos(theta) * Math.sin(phi);
+            newWordDict.cy = radius * Math.sin(theta) * Math.sin(phi);
+            newWordDict.cz = radius * Math.cos(phi);
+            newWordDict.h = 20
+            newWordDict.w = 20
+            newWordDict.per = 2 * radius
+                / (2 * radius + newWordDict.cz);
+
+            newWordDict.x = newWordDict.cx * newWordDict.per
+            newWordDict.y = newWordDict.cy * newWordDict.per
+            newCoordinates.push(newWordDict)
+        }
+
+        setCoordinates(newCoordinates)
+
+    }, [])
+
+
+
+
+
+  
+
+       
+
+        useEffect(() => {
+            const interval =   setInterval(() => {
+                let phi = 0;
+                let theta = 0;
+                let max = words.length;
+    
+                let newCoordinates = []
+                let rx1, ry1, rz1
+    
+    
+                for (const ix in coordinates) {
+    
+                    const i = parseInt(ix)
+    
+                    rx1 = coordinates[i].cx;
+                    ry1 = coordinates[i].cy * cy + coordinates[i].cz * -sy;
+                    rz1 = coordinates[i].cy * sy + coordinates[i].cz * cy;
+    
+                    coordinates[i].cx = rx1 * cx + rz1 * sx;
+                    coordinates[i].cy = coordinates[i].cy * cy + coordinates[i].cz * -sy;
+                    coordinates[i].cz = rx1 * -sx + rz1 * cx;
+    
+                    var per = 2 * radius
+                        / (2 * radius + coordinates[i].cz);
+                    coordinates[i].x = coordinates[i].cx * per;
+                    coordinates[i].y = coordinates[i].cy * per;
+                    coordinates[i].alpha = per / 2;
+                    coordinates[i].per = per
+    
+                    
+                }
+    
+                setCoordinates([...coordinates])
+            }, 30)
+        
+            // Cleanup: Clear the interval when the component unmounts
+            return () => {
+              clearInterval(interval);
+            };
+          }, []);
+
+    return <div className="text-xl self-center m-24 relative" >
+
+
+        {words.map((x, i) =>
+            <div className="m-2 absolute " style={{
+                left: coordinates[i]?.x,
+                top: radius + coordinates[i]?.y,
+                opacity: coordinates[i]?.per / 2,
+                fontSize: `${coordinates[i]?.per * 4 + 10}px`
+            }}> {x} </div>
+        )}
+
+    </div>
+}
