fe1fbb69e9986fce7a8299143f9e64eaaedf70e6
Muratcan Aşgün <masgun13@ku.edu.tr>
2024-07-26 08:08:21 +0300

    main page visual update

--- app/page.js
+++ app/page.js
@@ -102,16 +102,18 @@ export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center place-content-start p-0 ${cairo.className}`}>
 
-      <div className="bg-gray-200 z-10 mb-12 w-full  items-center justify-center    text-sm lg:flex flex-col">   {/* max-w-5xl*/}
-        <p className="  left-0 top-0 flex w-full justify-center border-b
-         pb-6 pt-8 mt-64 mb-64 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  
+      <div className="bg-gray-200 z-10  w-full  items-center justify-center    text-sm lg:flex flex-col">   {/* max-w-5xl*/}
+        <div className="flex flex-col items-center lg:flex-row left-0 top-0 flex w-full justify-center border-b
+         pb-6 pt-8 mt-32 mb-96 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  
         lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"> {/* fixed bg-gradient-to-b   border-gray-300 
           from-zinc-200 */}
 
-          <span className="  font-bold text-4xl">Etymallogy</span>
-        </p>
+          <span className="lg:mr-32  font-bold text-4xl">Etymallogy</span>
+          <Etymoball words={["şerbet", "şarap", "meclis", "wine", "şurup", "語", "kitap", "lycée", "bilim", "science",
+            "لغة", "vin", "sorbetto", "λόγος", "lisan", "ستاره", "stella", "ἀστήρ"]}></Etymoball>
+            </div>
 
-        <div className="bg-slate-300 left-0 right-0 w-full flex flex-col h-[150svh]">
+        <div className="bg-slate-300 left-0 right-0 w-full flex flex-col h-[100svh]">
           <div className="bg-gradient-to-b from-gray-200 h-1/3 flex flex-col justify-center">
               <SearchBar searchHandle={searchHandle} 
                           searchText={searchText}
@@ -121,15 +123,14 @@ export default function Home() {
                           noDataFound={noDataFound}
                           maxSearchResults={maxSearchResults}
                           dropdownItemClickHandle={dropdownItemClickHandle} ></SearchBar>
-            <p className=" text-xl self-center m-24 ">An online collection of etymological knowledge</p>
+            <p className=" text-xl self-center m-8 lg:m-24 text-center ">An online collection of etymological knowledge</p>
 
 
             <div className=" text-xl self-center m-4 ">1000+ Turkish words</div>
             <div className=" text-xl self-center m-4 ">1000+ English words</div>
             <div className=" text-xl self-center m-4 ">1000+ Arabic words</div>
           </div>
-          <Etymoball words={["şerbet", "şarap", "meclis", "wine", "şurup", "語", "kitap", "lycée", "bilim", "science",
-            "لغة", "vin", "sorbetto", "λόγος", "lisan", "ستاره", "stella", "ἀστήρ"]}></Etymoball>
+          
         </div>
 
 
@@ -150,7 +151,7 @@ const SearchBar = ({searchHandle, searchText, isSearchDropdownOpen, resetSearch,
     <form>
       <input
         type="text"
-        className="self-center w-full placeholder-gray-400 text-gray-900 p-4 rounded-t-xl outline-0"
+        className="self-center w-full placeholder-gray-400 text-gray-900 p-4 rounded-t-xl outline-0 text-lg"
         placeholder="Search for a word"
         onChange={searchHandle}
         value={searchText}
