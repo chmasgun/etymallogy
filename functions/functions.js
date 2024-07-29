05c5bfe861e735676a8714c85e2eb447b8b4483e
Muratcan Aşgün <masgun13@ku.edu.tr>
2024-07-29 16:24:43 +0300

    separated tree functions into a new file

--- functions/functions.js
+++ functions/functions.js
@@ -22,12 +22,12 @@ const langColors = {
 }
 
 
-const DrawRelation = ({ x1, x2, heightOffset, y, depthDiff, pair, setHoveredPair, isInsertMode, setIsInsertMode, highlightedWords  }) => {
+const DrawRelation = ({ x1, x2, heightOffset, y, depthDiff, pair, setHoveredPair, isInsertMode, setIsInsertMode, highlightedWords }) => {
 
     const [lineColor, setLineColor] = useState("#777")
     const [lineWidth, setLineWidth] = useState(1)
     const [clicked, setClicked] = useState(false)
-    const [lineOpacity, setLineOpacity] = useState( highlightedWords.includes(pair[0]) & highlightedWords.includes(pair[1]) ? 1 : 0.1)
+    const [lineOpacity, setLineOpacity] = useState(highlightedWords.includes(pair[0]) & highlightedWords.includes(pair[1]) ? 1 : 0.1)
 
     const setHoverColor = () => {
         setLineColor("#f77")
@@ -82,7 +82,7 @@ const DirectLine = ({ x, heightOffset, y, lineColor, lineWidth, lineOpacity, set
                 markerHeight="6"
                 markerUnits={"userSpaceOnUse"}
                 orient="auto-start-reverse"
-                
+
                 stroke={lineColor}
                 fill={lineColor}
             >
@@ -124,10 +124,10 @@ const SteppedLine = ({ x, heightOffset, y, lineColor, lineWidth, lineOpacity, se
                 markerWidth="6"
                 markerHeight="6"
                 markerUnits={"userSpaceOnUse"}
-                orient="auto-start-reverse" 
+                orient="auto-start-reverse"
                 stroke={lineColor}
                 fill={lineColor}
-                
+
             >
 
                 <path d="M 0 0 L 10 5 L 0 10 z" />
@@ -288,4 +288,170 @@ async function FetchSearchWords(textkey) {
     return wordsData
 }
 
-export { DrawRelation, langColors, RecalculateDepthAfter, FetchSearchWords }
+const calculateWidthBelowNode = (data, nodeList) => {
+
+    if (nodeList.length === 0) {
+        return 1
+    }
+    else {
+
+        let result = 0
+        for (const node of nodeList) {
+            let derivesTo = data[node]["rel"]["derives"]["to"] || []
+            let loansTo = data[node]["rel"]["loans"]["to"] || []
+            let homonymTo = data[node]["rel"]["homonym"]["to"] || []
+
+            let goToItems = derivesTo.concat(loansTo, homonymTo)
+            result += calculateWidthBelowNode(data, goToItems)
+        }
+        return result
+    }
+}
+
+const prepareWidthBelowNode = (data) => {
+    const widthBelowDict = {}
+    for (const node in data) {
+        widthBelowDict[node] = calculateWidthBelowNode(data, [node])
+    }
+    console.log(widthBelowDict);
+    return widthBelowDict
+}
+const calculatePositions = (data, wordWidth, totalDepth,leftPixelLimit) => {
+
+    // Part 0 Define parent offset dictionary. This is used in aligning children properly, in the case of adjusted parent (after unbalanced children offset)
+    const parentOffset = {}
+    // Part 1 Recursively calculate width needed for each node
+    const widthBelowDict = prepareWidthBelowNode(data)
+    console.log(widthBelowDict);
+
+
+    // Part 2 Pick the root and the children
+    let returnDict = {}
+    let root = data.filter(x => x.depth === 0)
+    let depthItems = root.length
+    //console.log(["SELECTED ", data]);
+    //console.log(root[0]["rel"]["derives"]["to"])
+    let idnow = root[0]["id"]
+    returnDict[idnow] = 0 / 2 - wordWidth / 2                  // ASSIGN ROOT TO CENTER. TO DO HANDLE MULTIPLE ROOTS
+    let derivesTo = root[0]["rel"]["derives"]["to"] || []
+    let loansTo = root[0]["rel"]["loans"]["to"] || []
+    let homonymTo = root[0]["rel"]["homonym"]["to"] || []
+
+    let goToItems = [derivesTo.concat(loansTo, homonymTo)]
+    let parentPositions = [0 / 2 - wordWidth / 2]
+    let parentWidths = [widthBelowDict[idnow]]
+    let removedParentOffsets = [0]
+    // Part 3 Iterate over the children
+    let safety = 0
+    while (goToItems.length > 0) {
+
+        let idsToProcess = goToItems[0]
+        let parentPos = parentPositions[0]
+        let parentWidth = parentWidths[0]
+        let itemTogetherCount = idsToProcess.length
+        let removedParentOffset = removedParentOffsets[0]
+        let extraSpaceNeeded = 0
+        for (var idNow = 0; idNow < itemTogetherCount; idNow++) {  // Treat the similar siblings together (e.g derived from same root)
+            const widthForThisSibling = widthBelowDict[idsToProcess[idNow]] || 1
+
+            let nodeData = data[idsToProcess[idNow]]//data.filter(x => x.id === idsToProcess[idNow])
+            let derivesTo = nodeData["rel"]["derives"]["to"] || []
+            let loansTo = nodeData["rel"]["loans"]["to"] || []
+            let homonymTo = nodeData["rel"]["homonym"]["to"] || []
+
+            let newGoToItems = derivesTo.concat(loansTo, homonymTo)
+            // small fix on parent, based on different width children
+            const firstAndLastChildren = [newGoToItems[0], newGoToItems[newGoToItems.length - 1]]
+            //console.log([idsToProcess[idNow], firstAndLastChildren]);
+            let additionalChildrenOffset = widthBelowDict[firstAndLastChildren[0]] - widthBelowDict[firstAndLastChildren[1]] || 0
+            parentOffset[idsToProcess[idNow]] = additionalChildrenOffset
+
+            let finalOffsetForWidthImbalance = (additionalChildrenOffset - removedParentOffset) / 2  // remove parent's offset
+            returnDict[idsToProcess[idNow]] = parentPos + (extraSpaceNeeded + (finalOffsetForWidthImbalance + widthForThisSibling - parentWidth) / 2 + idNow) * wordWidth * 1.2  // assign their left values
+
+            extraSpaceNeeded += (widthForThisSibling - 1)
+
+            if (newGoToItems.length > 0) {
+
+                goToItems.push(newGoToItems)
+                parentPositions.push(returnDict[idsToProcess[idNow]])
+                parentWidths.push(widthBelowDict[idsToProcess[idNow]])
+                removedParentOffsets.push(parentOffset[idsToProcess[idNow]])
+            }
+
+        }
+        goToItems.shift()           // remove the first element
+        parentPositions.shift()     // remove the first element
+        parentWidths.shift()
+        removedParentOffsets.shift()
+    }
+    const leftOverflow = leftPixelLimit - Math.min(...Object.keys(returnDict).map(x => returnDict[x]))
+    if (leftOverflow > 0) {
+
+        for (const node of Object.keys(returnDict)) {
+            console.log([node, returnDict]);
+            returnDict[node] = returnDict[node] + leftOverflow
+        }
+    }
+    const maxLeftValue = Math.max(...Object.keys(returnDict).map(x => returnDict[x]))
+    return [returnDict, maxLeftValue]
+}
+// TO DO for each depth, go over all nodes, extract from-to relations, and calculate xs and ys
+const calculateLines = (data, wordWidth, wordHeight, depthWidth, totalDepth, positionDict) => {
+    let outLinesList = []
+    let outLineNodes = []
+    for (var depthNow = 0; depthNow <= totalDepth; depthNow++) {  // iterating each depth
+        let root = data.filter(x => x.depth === depthNow)
+        let linesForDepth = []
+        let nodesForDepth = []
+        for (var elt of root) { // iterating each element/word
+            let derivesTo = elt["rel"]["derives"]["to"] || []
+            let loansTo = elt["rel"]["loans"]["to"] || []
+            let homonymTo = elt["rel"]["homonym"]["to"] || []
+            let newGoToItems = derivesTo.concat(loansTo, homonymTo)
+
+            for (var rel of newGoToItems) { // iterating every relation to gather lines
+                linesForDepth.push([positionDict[elt["id"]] + wordWidth / 2, positionDict[rel] + wordWidth / 2, wordHeight])
+                nodesForDepth.push([elt["id"], rel])
+            }
+
+            //console.log(newGoToItems);
+        }
+        outLinesList.push(linesForDepth)
+        outLineNodes.push(nodesForDepth)
+    }
+    return [outLinesList, outLineNodes]
+}
+const findHighlightedWords = (wordData, filteredData, setHighlightedWords) => {
+
+    let newHighlightedWords = [wordData.id]
+
+    let derivesFrom = wordData["rel"]["derives"]["from"] || []
+    let loansFrom = wordData["rel"]["loans"]["from"] || []
+    let homonymFrom = wordData["rel"]["homonym"]["from"] || []
+
+    let goToItems = derivesFrom.concat(loansFrom, homonymFrom)
+    while (goToItems.length > 0) {
+        let nodeData = filteredData[goToItems[0]]//data.filter(x => x.id === idsToProcess[idNow])
+        console.log(filteredData, goToItems, nodeData);
+        derivesFrom = nodeData["rel"]["derives"]["from"] || []
+        loansFrom = nodeData["rel"]["loans"]["from"] || []
+        homonymFrom = nodeData["rel"]["homonym"]["from"] || []
+
+        let newGoToItems = derivesFrom.concat(loansFrom, homonymFrom)
+        newHighlightedWords.push(goToItems[0])
+        goToItems.push(...newGoToItems)
+
+        goToItems.shift()
+    }
+
+    setHighlightedWords([...newHighlightedWords])
+
+
+}
+
+export {
+    DrawRelation, langColors, RecalculateDepthAfter, FetchSearchWords,
+    calculateWidthBelowNode, prepareWidthBelowNode, calculateLines, calculatePositions,
+    findHighlightedWords
+}
